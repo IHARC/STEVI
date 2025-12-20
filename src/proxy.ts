64c4ea4f030c4d5efdf62ada8e6724095595f053
IHARC-DEV <jordan@iharc.ca>
2025-12-20 02:34:46 -0500

    build fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -60,5 +60,7 @@ export async function proxy(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/((?!_next/|favicon\.ico|robots\.txt|sitemap\.xml|assets/|static/).*)'],
+  matcher: [
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|assets|static).*)',
+  ],
 };
